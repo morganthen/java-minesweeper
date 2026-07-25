f8c45b45968396811bbb2f32a3f2e90a9240f872
morganthen <morganthen@me.com>
2026-07-25 21:33:15 +0800

    Adds checkWin method to Board class

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -7,8 +7,16 @@ import board.Cell;
 
 public class Minesweeper {
 
-    static void renderBoard(Board board){
-            Cell[][] grid = board.getGrid();
+    Board board;
+    Scanner scanner;
+
+    public Minesweeper(){
+        this.board = new Board();
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void renderBoard(){
+            Cell[][] grid = this.board.getGrid();
             for (int r = 0; r <= 10; r++){
                 for(int c = 0; c < 10 ; c++){
                     if (r == 10){
@@ -36,25 +44,39 @@ public class Minesweeper {
             System.out.println(" 1  2  3  4  5  6  7  8  9  10 ");
         }
 
-        public void checkWin(){
+        public boolean checkWin(){
+            int revealedCount = 0;
+            Cell[][] grid = this.board.getGrid();
+            for (int r = 0 ; r < 10; r++){
+                for (int c= 0; c < 10 ; c++){
+                    if(grid[r][c].isRevealed()) revealedCount++;
+                }
+            }
+
+            return revealedCount == 90;
+
 
         }
 
         public void start(){
             System.out.println("WELCOME TO MINESWEEPER!");
             System.out.println("LET'S PLAY!");
-            Scanner scanner = new Scanner(System.in);
-            Board board = new Board();
+            // game loop starts here
             while(true){
-            renderBoard(board);
-            System.out.println("Select row");
-            int row = scanner.nextInt() - 1;
-            System.out.println("Select column");
-            int col = scanner.nextInt() - 1;
-            if(board.reveal(row, col)){
-                System.out.println("BOOM YOU LOSE!");
-                break;
-            };
+                renderBoard();
+                System.out.println("Select row");
+                int row = this.scanner.nextInt() - 1;
+                System.out.println("Select column");
+                int col = this.scanner.nextInt() - 1;
+                if(board.reveal(row, col)){
+                    System.out.println("BOOM YOU LOSE!");
+                    break;
+                };
+                if(checkWin()){
+                    renderBoard();
+                    System.out.println("YOU WIN!");
+                    break;
+                };
             
         }
 
