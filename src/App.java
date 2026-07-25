e622c45d888e6712f5cc5c686768326e324abc23
morganthen <morganthen@me.com>
2026-07-25 18:51:45 +0800

    adds calculate adjacent mines method in board class

--- src/App.java
+++ src/App.java
@@ -5,22 +5,45 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("WELCOME TO MINESWEEPER!");
         System.out.println("LET'S PLAY!");
-
         Board board = new Board();
         renderBoard(board);
 
        
     }
-
+    //display or render board
      static void renderBoard(Board board){
             Cell[][] grid = board.getGrid();
-            for (int r = 0; r < 10; r++){
+            for (int r = 0; r <= 10; r++){
                 for(int c = 0; c < 10 ; c++){
-                    if (!grid[r][c].isRevealed())
-                    System.out.print("[.]");
-            
+                    if (r == 10){
+                        System.out.print(" - ");
+                    }else{
+
+                        if (!grid[r][c].isRevealed()){
+                            System.out.print("[.]");
+                            
+                        }else if (grid[r][c].isMine()){
+                            System.out.print("[*]");
+                        }else{
+                            int count = grid[r][c].getAdjacentMines();
+                            if(count == 0){
+                                
+                                System.out.print("[ ]");
+                            }else{
+                                System.out.print("[" + count + "]");
+                                
+                            }
+                        }
+                    }
+
+                    
+                
+                }
+                if(r < 10){
+                    System.out.print("| " + (r+1));
                 }
                 System.out.println();
             }
+            System.out.println(" 1  2  3  4  5  6  7  8  9  10 ");
         }
 }
--- src/board/Board.java
+++ src/board/Board.java
@@ -2,7 +2,7 @@ package board;
 
 public class Board {
 
-    private Cell[][] grid = new Cell[10][10];
+    private Cell[][] grid = new Cell[10][10]; //new array of TYPE cell duhhh (no cell obejct has been created YET!!!)
 
     public Board(){
         for (int r = 0; r < 10; r++){
@@ -10,7 +10,55 @@ public class Board {
                 grid[r][c] = new Cell(false, false, 0);
             }
         }
+        //place mines then...
+        placeMines();
+        //...calculate adjacent mines
+        calculateAdjacentMines();
     }
+
+    public void placeMines(){
+        for (int i = 0; i < 10; i++){
+            int row = (int)(Math.random() * 10);  
+            int col = (int)(Math.random() * 10);
+            while(grid[row][col].isMine()){
+                    row = (int)(Math.random() * 10);  
+                    col = (int)(Math.random() * 10);
+                }
+                grid[row][col].setMine(true);
+            
+        }
+
+
+    }
+
+    public void calculateAdjacentMines(){
+
+        
+        for (int r = 0; r < 10; r++){
+            for (int c = 0; c < 10; c++){
+                if (grid[r][c].isMine()) continue;
+                int count = 0; 
+                for (int dr = -1; dr <= 1; dr++){
+                    for (int dc= -1; dc <= 1; dc++){
+                        if(dr == 0 && dc == 0) continue; //skip self
+                        int nr = r + dr;
+                        int nc = c + dc;
+                        if(nr >= 0 && nc < 10 && nc >=0 && nr < 10){
+                            if(grid[nr][nc].isMine()) {
+                                count++;
+                            };
+                        }
+                    }
+                }
+                grid[r][c].setAdjacentMines(count);
+            }
+
+        }
+
+        
+    }
+
+
 
 
     public Cell[][] getGrid() {
